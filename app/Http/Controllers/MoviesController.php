b02385551f8934958207af2ff5cfcc25c803655d
tahmid010 <tahmid11q@gmail.com>
2020-11-22 13:03:11 +0600

    Now Playing Section and Movie Details Page Updated

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -69,7 +69,16 @@ class MoviesController extends Controller
      */
     public function show($id)
     {
-        return view('show');
+
+        $movie = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/'.$id.'?append_to_response=credits,videos,images')
+            ->json();
+
+        dump($movie);
+
+        return view('show', [
+            'movie' => $movie,
+        ]);
     }
 
     /**
