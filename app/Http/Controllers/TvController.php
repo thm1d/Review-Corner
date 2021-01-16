4c0e2c1eb01150c5f6075e5c39036cdc9300642e
thm1d <tahmid11q@gmail.com>
2021-01-16 18:21:51 +0600

    Authentication added with Breeze

--- app/Http/Controllers/TvController.php
+++ app/Http/Controllers/TvController.php
@@ -79,7 +79,7 @@ class TvController extends Controller
     public function show($id)
     {
         $tvShow = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/tv/'.$id.'?append_to_response=credits,videos,images,review')
+            ->get('https://api.themoviedb.org/3/tv/'.$id.'?append_to_response=credits,videos,images,review,external_ids')
             ->json();
 
         //dump($movie);
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,22 @@ use App\Http\Controllers\MoviesController;
 use App\Http\Controllers\ActorController;
 use App\Http\Controllers\TvController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
 
 Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
 Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show');
@@ -16,4 +32,3 @@ Route::get('/actors', [ActorController::class, 'index'])->name('actors.index');
 Route::get('/actors/page/{page?}', [ActorController::class, 'index'])->name('actors.index');
 
 Route::get('/actors/{actor}', [ActorController::class, 'show'])->name('actors.show');
-
